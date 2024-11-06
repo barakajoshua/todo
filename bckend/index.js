996cc7d2facc80231e263ed68a07956c1486fd7a
barakajoshua <balakajoshua@gmail.com>
2024-11-06 16:54:31 +0200

    fixed connection to mongo db

--- bckend/index.js
+++ bckend/index.js
@@ -6,8 +6,14 @@ const route = require('./routes/router')
 app.use(route)
 
 
-const connection = mongoose.connect('mongodb://localhost:27017/todo/mytodo')
-
+try {
+       const connection = mongoose.connect('mongodb://localhost:27017/todo/mytodo')
+       if(connection){
+              console.log("connected successfully")
+       }
+} catch (error) {
+       
+}
 
 app.listen(5000,(req,res) =>{
        console.log('server running on port http://localhost:5000')
